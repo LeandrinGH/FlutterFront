67a383f645b1d670caaff12d9b49e1cfd0eeb721
LeandroFreireGH <leandro.freire@ucuenca.edu.ec>
2025-06-22 11:20:04 -0500

    LF-Implements screaming architecture in the project

---
+++ front_crud_users/lib/features/users/domain/entities/user.dart
@@ -0,0 +1,12 @@
+class User {
+    String username;
+    String email;
+    String password;
+
+    User({
+        required this.username,
+        required this.email,
+        required this.password,
+    });
+
+}
